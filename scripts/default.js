0279506aa080a2a058762a27927ec399c5fcfe44
jstsmthrgk <38539927+jstsmthrgk@users.noreply.github.com>
2018-11-22 21:38:22 +0100

    mobile navigation now closable from everywhere

--- scripts/default.js
+++ scripts/default.js
@@ -1,5 +1,13 @@
 function togglenav() {
 	document.querySelector("body > header > nav").classList.toggle("show");
+	return false;
+}
+
+function hidenav(e) {
+	if (document.querySelector("body > header > nav").classList.contains("show")) {
+		document.querySelector("body > header > nav").classList.remove("show");
+		e.preventDefault();
+	}
 }
 
 function compute() {
@@ -11,5 +19,7 @@ function compute() {
 }
 
 document.querySelector("body > header > nav").classList.add("hide");
+document.querySelector("body > main").addEventListener("click", hidenav);
+document.querySelector("body > footer").addEventListener("click", hidenav);
 window.addEventListener("resize", compute);
 compute();
