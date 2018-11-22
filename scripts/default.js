cddc1dfd1fa228f7ae0fc0f8ab19d5c77f29b269
jstsmthrgk <38539927+jstsmthrgk@users.noreply.github.com>
2018-11-22 19:51:17 +0100

    Redesign

--- scripts/default.js
+++ scripts/default.js
@@ -1,20 +1,15 @@
 function togglenav() {
-	if(document.querySelector("body > nav").style.display == "block") {
-		document.querySelector("body > nav").style.setProperty("display", "none", "important");
-	} else {
-		document.querySelector("body > nav").style.setProperty("display", "block", "important");
-	}
+	document.querySelector("body > header > nav").classList.toggle("show");
 }
 
 function compute() {
 	if(window.innerHeight > window.innerWidth) {
-		document.querySelector("body > nav").style.setProperty("display", "none", "important");
-		document.querySelector("body > header").addEventListener("click", togglenav);
+		document.querySelector("body > header > h1").addEventListener("click", togglenav);
 	} else {
-		document.querySelector("body > nav").style.setProperty("display", "block", "important");
-		document.querySelector("body > header").removeEventListener("click", togglenav);
+		document.querySelector("body > header > h1").removeEventListener("click", togglenav);
 	}
 }
 
+document.querySelector("body > header > nav").classList.add("hide");
 window.addEventListener("resize", compute);
 compute();
