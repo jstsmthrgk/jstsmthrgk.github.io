a296121d6a8201a1201e219725d5181ed78ac39d
Gabriel Eckertsberger <gabriel.eck@gmx.at>
2019-04-05 20:36:21 +0200

    redesign

--- scripts/default.js
+++ scripts/default.js
@@ -1,24 +1,24 @@
 function togglenav() {
-	document.querySelector("body > header").classList.toggle("nav-show");
+	document.querySelector("body > nav").classList.toggle("nav-show");
 	return false;
 }
 
 function hidenav(e) {
-	if (document.querySelector("body > header").classList.contains("nav-show")) {
-		document.querySelector("body > header").classList.remove("nav-show");
+	if (document.querySelector("body > nav").classList.contains("nav-show")) {
+		document.querySelector("body > nav").classList.remove("nav-show");
 		e.preventDefault();
 	}
 }
 
 function compute() {
 	if(window.innerHeight > window.innerWidth) {
-		document.querySelector("body > header > h1").addEventListener("click", togglenav);
+		document.querySelector("body > header").addEventListener("click", togglenav);
 	} else {
-		document.querySelector("body > header > h1").removeEventListener("click", togglenav);
+		document.querySelector("body > header").removeEventListener("click", togglenav);
 	}
 }
 
-document.querySelector("body > header").classList.add("nav-hide");
+document.querySelector("body > nav").classList.add("nav-hide");
 document.querySelector("body > main").addEventListener("click", hidenav);
 document.querySelector("body > footer").addEventListener("click", hidenav);
 window.addEventListener("resize", compute);
